6424dae01d99d34466c2318c0fad7bae298c695b
Atharva Kumkar <kumkaratharva8@gmail.com>
2025-06-23 22:31:43 +0530

    Create volumeofcircle.java

---
+++ volumeofcircle.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+      Scanner input = new Scanner(System.in);
+
+     double radius;
+     double volume;
+
+     System.out.print("Enter the radius of circle: ");
+     radius =  input.nextDouble();
+
+     volume = (4.0/3.0) * Math.PI * Math.pow(radius, 3);
+     System.out.print("The volume of circle is: " + volume);
+
+
+      input.close();
+    }
+}
